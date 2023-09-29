821cd71015ee9e5a06925e8ad12c263a5a487903
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-09-28 20:35:01 -0700

    🎨 Format with Styler

--- tests/testthat/test_rolling_mean.R
+++ tests/testthat/test_rolling_mean.R
@@ -1,12 +1,12 @@
 describe("Rolling mean", {
   it("First example: xG", {
-    data <- tibble::tibble(xG = c(1,2,1,2,1))
+    data <- tibble::tibble(xG = c(1, 2, 1, 2, 1))
     expected <- c(1.5, 1.5)
     obtained <- xg_rolling_mean(data)
     expect_equal(obtained, expected)
   })
   it("Example: PPDA", {
-    data <- tibble::tibble(PPDA = c(1,2,3,4,5))
+    data <- tibble::tibble(PPDA = c(1, 2, 3, 4, 5))
     expected <- c(2.5, 3.5)
     obtained <- ppda_rolling_mean(data)
     expect_equal(obtained, expected)
