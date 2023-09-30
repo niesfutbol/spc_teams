97abcfe688c079440e1c58460309d693ca514b3c
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-09-29 20:54:56 -0700

    🛑🧪 Test `shuffle()`: The elements are the same

--- tests/testthat/test-control_chart.R
+++ tests/testthat/test-control_chart.R
@@ -4,4 +4,10 @@ describe("Resort data", {
     obtained <- shuffle(data)
     expect_false(all(data == obtained))
   })
+  it("All elements are the same", {
+    data <- seq(10)
+    obtained <- shuffle(data)
+    expect_true(all(obtained %in% data))
+    expect_true(all(data %in% obtained))
+  })
 })
