53081a30216bd69cf76f89a9092959b5d297f993
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-09-29 21:15:39 -0700

    🪲🛑 Use sample for tibble

--- tests/testthat/test-control_chart.R
+++ tests/testthat/test-control_chart.R
@@ -1,13 +1,13 @@
 describe("Resort data", {
   it("The data are the same but in different order", {
-    data <- seq(10)
+    data <- tibble::tibble(a = seq(10), b = letters[1:10])
     obtained <- shuffle(data)
-    expect_false(all(data == obtained))
+    expect_false(all(data[["a"]] == obtained[["a"]]))
   })
   it("All elements are the same", {
-    data <- seq(10)
+    data <- tibble::tibble(a = seq(10), b = letters[1:10])
     obtained <- shuffle(data)
-    expect_true(all(obtained %in% data))
-    expect_true(all(data %in% obtained))
+    expect_true(all(obtained[["a"]] %in% data[["a"]]))
+    expect_true(all(data[["a"]] %in% obtained[["a"]]))
   })
 })
